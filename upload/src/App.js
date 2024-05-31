6cbaf341ed833111e41ff4e529511ca0cba5c4e7
Obaid ur Rehman <obaid@obaid.digital>
2024-05-31 20:49:00 +0500

    all files added

---
+++ upload/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Checkout from "./components/checkout/Checkout";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Checkout />
+      </header>
+    </div>
+  );
+}
+
+export default App;
